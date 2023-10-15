dcc18f0449edefb721368cf0f3806430b6c75a7e
Jason Calalang <jcalalang@ph.ibm.com>
2023-10-15 23:35:38 +0800

    check digit implementation

--- test_check_digit.py
+++ test_check_digit.py
@@ -1,7 +1,21 @@
 import unittest
 
 def is_valid_luhn(number: str) -> bool:
-    pass  # TODO: Implement this function
+    if not number.isdigit():
+        raise ValueError("Input must be a numeric string")
+        
+    digits = [int(digit) for digit in number]
+    checksum = 0
+
+    # Double every second digit from the right and add the individual digits to the checksum
+    for i in range(-2, -len(digits) - 1, -2):
+        double_digit = digits[i] * 2
+        checksum += double_digit if double_digit < 10 else double_digit - 9
+
+    # Add the other digits to the checksum
+    checksum += sum(digits[-1::-2])
+
+    return checksum % 10 == 0
 
 class TestLuhnAlgorithm(unittest.TestCase):
     
